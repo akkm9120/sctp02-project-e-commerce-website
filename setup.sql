e6ed9cdfb7c564e55a6d79d0c32a7e1266a97f7b
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-12-24 07:21:53 +0000

    modified sql

--- setup.sql
+++ setup.sql
@@ -5,3 +5,10 @@ grant all privileges on *.* to 'noname'@'%';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE organic;
+
+use organic;
+
+INSERT into categories (id,name)
+VALUES 
+    (1,'Vegan'),
+    (2,'Non-Vegan');
