604bdfe15a8e389d155bace1db55b786071c2dfd
Min <150890227+akkm9120@users.noreply.github.com>
2025-03-17 21:03:36 +0800

    migration postgressql

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,18 +1,16 @@
 // knex is necessary for bookshelf to work
-const knex = require('knex')(
-    {
-        // client refers to what database technology we are using
-        client: process.env.DB_DRIVER,
-        connection: {
-            user: process.env.DB_USER,
-            password:process.env.DB_PASSWORD,
-            database:process.env.DB_DATABASE,
-            host:process.env.DB_HOST   // aka 'localhost'
-        }
-    }
-  );
-  
-  // create bookshelf
-  const bookshelf = require('bookshelf')(knex);
-  
-  module.exports= bookshelf;
+const knex = require("knex")({
+  client: process.env.DB_DRIVER,
+  connection: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    ssl: true,
+  },
+});
+
+// create bookshelf
+const bookshelf = require("bookshelf")(knex);
+
+module.exports = bookshelf;
--- index.js
+++ index.js
@@ -1,110 +1,119 @@
-const express = require('express');
-const hbs = require('hbs');
-const wax = require('wax-on');
-require('dotenv').config();
-const session = require('express-session');
-const flash = require('connect-flash');
-const FileStore = require('session-file-store')(session);
-const csurf = require('csurf');
-const cors = require('cors');
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+const session = require("express-session");
+const flash = require("connect-flash");
+const FileStore = require("session-file-store")(session);
+const csurf = require("csurf");
+const cors = require("cors");
 
 const app = express();
 
-
-const arrayOrigin = ['https://3000-akkm9120-project02front-7xbpwjll6s6.ws-us117.gitpod.io','https://3001-akkm9120-project02front-oebsnd1mauu.ws-us117.gitpod.io','https://3000-akkm9120-project02front-umtcl5bzv03.ws-us117.gitpod.io','https://akkm9120-project02front-umtcl5bzv03.ws-us117.gitpod.io/','https://project02-frontend.vercel.app','https://project02-frontend-j9ua0wl8d-akkm9120s-projects.vercel.app'] // Array of allowed origins
+const arrayOrigin = [
+  "https://3000-akkm9120-project02front-7xbpwjll6s6.ws-us117.gitpod.io",
+  "https://3001-akkm9120-project02front-oebsnd1mauu.ws-us117.gitpod.io",
+  "https://3000-akkm9120-project02front-umtcl5bzv03.ws-us117.gitpod.io",
+  "https://akkm9120-project02front-umtcl5bzv03.ws-us117.gitpod.io/",
+  "https://project02-frontend.vercel.app",
+  "https://project02-frontend-j9ua0wl8d-akkm9120s-projects.vercel.app",
+]; // Array of allowed origins
 // Enable CORS for all routes
-app.use(cors({
+app.use(
+  cors({
     origin: arrayOrigin,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 // Use hbs for the view engine
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // Enable the static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Enable wax-on for template inheritance
 wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts');
+wax.setLayoutPath("./views/layouts");
 
 // Enable forms
 app.use(express.urlencoded({ extended: false }));
 
 // Enable sessions
-app.use(session({
+app.use(
+  session({
     store: new FileStore(),
-    secret: 'SecretKey',
+    secret: "SecretKey",
     resave: false,
     saveUninitialized: true,
-}));
+  })
+);
 
 app.use(flash()); // Enable flash messages
 
 // Share the current logged-in user with all hbs files
 app.use(function (req, res, next) {
-    res.locals.user = req.session.user;
-    next();
+  res.locals.user = req.session.user;
+  next();
 });
 
 // Enable CSRF protection
 const csurfInstance = csurf();
 app.use(function (req, res, next) {
-    if (req.url.slice(0, 5) == '/api/') {
-        return next();
-    }
-    csurfInstance(req, res, next);
+  if (req.url.slice(0, 5) == "/api/") {
+    return next();
+  }
+  csurfInstance(req, res, next);
 });
 
 // Share the CSRF token with all hbs files
 app.use(function (req, res, next) {
-    if (req.csrfToken) {
-        res.locals.csrfToken = req.csrfToken();
-    }
-    next();
+  if (req.csrfToken) {
+    res.locals.csrfToken = req.csrfToken();
+  }
+  next();
 });
 
 // Handle CSRF errors
 app.use(function (err, req, res, next) {
-    if (err && err.code == "EBADCSRFTOKEN") {
-        req.flash("error_messages", "The form has expired, please try again");
-        res.redirect('back');
-    } else {
-        next();
-    }
+  if (err && err.code == "EBADCSRFTOKEN") {
+    req.flash("error_messages", "The form has expired, please try again");
+    res.redirect("back");
+  } else {
+    next();
+  }
 });
 
 // Routes
 async function main() {
-    const landingRoutes = require('./routes/landing');
-    const productRoutes = require('./routes/products');
-    const userRoutes = require('./routes/users');
-    const cloudinaryRoutes = require('./routes/cloudinary');
-    const shoppingCartRoutes = require('./routes/shoppingCart');
-    const checkoutRoutes = require('./routes/checkout');
+  const landingRoutes = require("./routes/landing");
+  const productRoutes = require("./routes/products");
+  const userRoutes = require("./routes/users");
+  const cloudinaryRoutes = require("./routes/cloudinary");
+  const shoppingCartRoutes = require("./routes/shoppingCart");
+  const checkoutRoutes = require("./routes/checkout");
 
-    const api = {
-        products: require('./routes/api/products'),
-        orders: require('./routes/api/order')
-    };
+  const api = {
+    products: require("./routes/api/products"),
+    orders: require("./routes/api/order"),
+  };
 
-    app.use('/', landingRoutes);
-    app.use('/products', productRoutes);
-    app.use('/users', userRoutes);
-    app.use('/cloudinary', cloudinaryRoutes);
-    app.use('/cart', shoppingCartRoutes);
-    app.use('/checkout', checkoutRoutes);
+  app.use("/", landingRoutes);
+  app.use("/products", productRoutes);
+  app.use("/users", userRoutes);
+  app.use("/cloudinary", cloudinaryRoutes);
+  app.use("/cart", shoppingCartRoutes);
+  app.use("/checkout", checkoutRoutes);
 
-    // RESTful API endpoints
-    app.use('/api/products', express.json(), api.products);
-    app.use('/api/orders', express.json(), api.orders);
-
+  // RESTful API endpoints
+  app.use("/api/products", express.json(), api.products);
+  app.use("/api/orders", express.json(), api.orders);
 }
 
 main();
 
-app.listen(3000, () => {
-    console.log("Server has started on port 3000");
+app.listen(process.env.PORT, 3000, () => {
+  console.log("Server has started on port 3000");
 });
